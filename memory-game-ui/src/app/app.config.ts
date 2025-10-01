505e7bd3641cf9cb5ff8d7a6a5b50cf994f40168
Reddy <nagendharreddy22@gmail.com>
2025-10-01 23:58:06 +0530

    👌 IMPROVE : modified configurations

--- memory-game-ui/src/app/app.config.ts
+++ memory-game-ui/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { jwtInterceptor } from './core/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideAnimations()
   ]
 };
--- memory-game-ui/src/app/app.routes.ts
+++ memory-game-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login';
+import { RegisterComponent } from './auth/register/register';
+import { GameComponent } from './game/game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'game', component: GameComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
