b5b05967a8d9a30cfcc9965a33a09ea011a01c30
Reddy <nagendharreddy22@gmail.com>
2025-10-03 08:13:49 +0530

    📦 NEW : added navbar and footer

--- memory-game-ui/src/app/app.routes.ts
+++ memory-game-ui/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
-import { GameComponent } from './game/game';
+import { Game } from './game/game';
+import { Profile } from './auth/profile/profile';
+import { Dashboard } from './auth/dashboard/dashboard';
+import { Logout } from './auth/logout/logout'
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'game', component: GameComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'game', component: Game, canActivate: [authGuard]  },
+  { path: 'profile', component: Profile, canActivate: [authGuard]  },
+  { path: 'logout', component: Logout },
+  { path: 'dashboard', component: Dashboard },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
